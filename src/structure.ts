0b6fece0fed1874595d875879277dda74d6e9e5b
shino <shino.hinaduki@gmail.com>
2021-10-08 03:40:26 +0900

    install typedoc

--- src/structure.ts
+++ src/structure.ts
@@ -4,14 +4,31 @@ import { Vec3 } from 'vec3';
 
 const mcAssets = require('minecraft-assets')('1.8.8');
 
+/**
+ * Indicates the original data of the building
+ */
 export class Structure {
     filepath: string;
     schematic: any;
+
+    /**
+     * Constructor
+     */
     constructor() {}
+
+    /**
+     * Load the schematic file
+     * @param path .schematic file path
+     */
     async load(path: string) {
         this.filepath = path;
         this.schematic = await Schematic.read(await readFile(path));
     }
+
+    /**
+     * Returns the building size
+     * @returns building size
+     */
     size(): { x: number; y: number; z: number } {
         const s = this.schematic.start();
         const e = this.schematic.end();
